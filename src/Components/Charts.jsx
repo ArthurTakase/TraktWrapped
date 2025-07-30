1a3d521074d96c0ac85a89cf73dae3bba54f1036
Arthur <manu@Mac-mini-de-Manu.local>
2025-07-30 23:22:41 +0200

    FEAT: charts

--- src/Components/Charts.jsx
+++ src/Components/Charts.jsx
@@ -7,8 +7,13 @@ import { allRef } from '../App'
 import '../scss/charts.scss'
 import 'cal-heatmap/cal-heatmap.css';
 import Heatmap from './Charts/Heatmap'
+import { Graph, PieGraph } from './Charts/Graph'
+import { WrappedData } from './Wrapped'
 
 export default function Charts() {
+    const [genresData, setGenresData] = useState([])
+    const [yearsData, setYearsData] = useState([])
+
     const closeCharts = () => {
         document.querySelector('.charts-container').classList.toggle('active')
         allRef.grid.current.style.display = 'flex'
@@ -18,16 +23,37 @@ export default function Charts() {
     }
     allRef.closeCharts = closeCharts
 
+    const processData = () => {
+        const genres = Object.entries(WrappedData.genres).map(([name, value]) => ({ name, value }))
+            .sort((a, b) => b.value - a.value)
+            .filter(genre => genre.value > 2)
+            .sort((a, b) => a.value - b.value)
+        genres.forEach(genre => {
+            if (genre.name.length > 10)
+                genre.name = genre.name.slice(0, 10) + '...'
+        })
+        setGenresData(genres)
+
+        const years = Object.entries(WrappedData.airing_dates).map(([name, value]) => ({ name, value }))
+            .sort((a, b) => b.value - a.value)
+            .slice(0, 10)
+            .sort((a, b) => a.name - b.name)
+        setYearsData(years)
+    }
+
     useEffect(() => {
         const close = (e) => { if (e.key === 'Escape') closeCharts() }
         document.addEventListener('keydown', close)
+        processData()
         return () => { document.removeEventListener('keydown', close) }
     }, [])
 
     return (
         <div className="charts-container active">
             <div className="body">
-                < Heatmap />
+                <Heatmap />
+                <Graph data={yearsData} />
+                <PieGraph data={genresData} />
             </div>
             <button className='close' onClick={closeCharts}><i className='bx bx-x' ></i></button>
         </div>
--- src/Components/Charts/Graph.jsx
+++ src/Components/Charts/Graph.jsx
@@ -1,20 +1,59 @@
 import React, { PureComponent } from 'react';
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Pie, PieChart } from 'recharts';
 
-export default function Graph({ data }) {
+export function Graph({ data }) {
+
+    console.log("Graph data:", data);
+
     return (
         <ResponsiveContainer width="100%" height={300}>
             <BarChart
                 data={data}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
             >
+                <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="0%" stopColor="#944abe" stopOpacity={1} />
+                        <stop offset="46%" stopColor="#9b45ac" stopOpacity={1} />
+                        <stop offset="76%" stopColor="#bd3563" stopOpacity={1} />
+                        <stop offset="100%" stopColor="#e33227" stopOpacity={1} />
+                    </linearGradient>
+                </defs>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="value" fill="#8884d8" />
+                <Bar dataKey="value" fill="url(#colorUv)" />
             </BarChart>
+        </ResponsiveContainer>
+    );
+}
+
+export function PieGraph({ data }) {
+
+    console.log("Graph data:", data);
+
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <PieChart width="100%" height="600px">
+                <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="0%" stopColor="#944abe" stopOpacity={1} />
+                        <stop offset="46%" stopColor="#9b45ac" stopOpacity={1} />
+                        <stop offset="76%" stopColor="#bd3563" stopOpacity={1} />
+                        <stop offset="100%" stopColor="#e33227" stopOpacity={1} />
+                    </linearGradient>
+                </defs>
+                <Pie
+                dataKey="value"
+                startAngle={180}
+                endAngle={0}
+                data={data}
+                fill="url(#colorUv)"
+                label
+                />
+                <Tooltip />
+            </PieChart>
         </ResponsiveContainer>
     );
 }
